ccac0c009259d11f017b2f81e3713f4f84b0b4bb
polyakovvladislav <polyakovvladislav@U11WKS02>
2017-03-13 08:25:50 +0100

    kopiop

--- WindowsFormsGithub2/Form1.cs
+++ WindowsFormsGithub2/Form1.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsGithub2
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("no");
+        }
     }
 }
